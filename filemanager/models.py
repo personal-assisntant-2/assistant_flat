e324ce46f114088bb3b87776b8d7d378f9991f82
Yana Anisimova <begemotvot2@gmail.com>
2021-11-15 15:24:48 +0200

    added category, delete note from edit_contact

--- filemanager/models.py
+++ filemanager/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 
+from .settings import FORMATS
+
 
 class UploadedFiles(models.Model):
     name = models.TextField(verbose_name="Name of file")
@@ -15,3 +17,21 @@ class UploadedFiles(models.Model):
         related_name="files"
     )
 
+    @property
+    def category(self):
+        """
+        File property, depending on the extension, sets the category according to the FORMATS dictionary.
+        """
+        category = None
+        for name_category, extensions in FORMATS.items():
+
+            # if self.extension in list extensions with key name_category,the category name is assigned.
+            if self.extension in extensions:
+                category = name_category
+
+        # If self.extension is not in the list of extensions with the name_category key,
+        # the category name 'Others' is assigned.
+        if category is None:
+            category = 'Others'
+
+        return category
